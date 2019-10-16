3cf1bf50e352b8290a4a2298b9a3f4277b8b0942
Jasmine Vout <47492753+jasminevout@users.noreply.github.com>
2019-10-16 08:39:06 +0800

    Initial Commit: Basic page template

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,29 @@
+<footer class="content-info">
+  <div class="container grid-x">
+    <!-- Text -->
+    <div class="footer-txt cell small-12 large-5">
+      <h2>
+        Friends of Battye Library Inc.
+      </h2>
+      <p>
+        Mission statement. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </div>
+    <!-- Menu 1 -->
+    <div class="footer-menu cell small-12 medium-6 large-2 large-offset-1">
+      <h3>About</h3>
+    </div>
+    <!-- Menu 2 -->
+    <div class="footer-menu cell small-12 medium-6 large-2">
+      <h3>Further Reading</h3>
+    </div>
+    <!-- Menu 3 -->
+    <div class="footer-menu cell small-12 medium-6 large-2">
+      <h3>Associates & Contributors</h3>
+    </div>
+
+    <div class="footer-copyright cell small-12">
+    © <?php echo date("Y"); ?> Friends of Battye Library Inc.
+    </div>
+  </div>
+</footer>
