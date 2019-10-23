61cdfe6cdbc978d7b81ea9d17a80f49a35e644e2
Jasmine Vout <47492753+jasminevout@users.noreply.github.com>
2019-10-23 12:18:28 +0800

    Add category archive template

---
+++ resources/views/partials/category-header.blade.php
@@ -0,0 +1,3 @@
+<div class="page-header">
+  <h1>@php single_cat_title() @endphp</h1>
+</div>
--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -1,13 +1,26 @@
 <article @php post_class() @endphp>
-  <header>
+  <header class="single-header">
+    <!-- Featured Image -->
+    <div class="entry-image">
+      @php the_post_thumbnail() @endphp
+    </div>
+    <!-- Post Title -->
     <h1 class="entry-title">{!! get_the_title() !!}</h1>
     @include('partials/entry-meta')
   </header>
+  <!-- Content -->
+  <div class="entry-excerpt">
+    <p>Article excerpt goes here.</p>
+  </div>
   <div class="entry-content">
     @php the_content() @endphp
   </div>
-  <footer>
-    {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
-  </footer>
-  @php comments_template('/partials/comments.blade.php') @endphp
+  <!-- Download -->
+  <div class="entry-download">
+    <p>Download PDF button here.</p>
+  </div>
+  <!-- More Articles -->
+  <div class="entry-morearticles">
+    <p>Show more articles here.</p>
+  </div>
 </article>
